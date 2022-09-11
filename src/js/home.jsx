57287d0564c6dc9bf2d72d6b95e749b296fdc18d
Harry <harryzhu45@gmail.com>
2022-09-10 22:15:18 -0700

    Update board

--- src/js/home.jsx
+++ src/js/home.jsx
@@ -2,30 +2,31 @@ import React from "react";
 import codejam_text from "../png/codejam-text.png";
 
 class Home extends React.Component {
-
   render() {
     return (
       <div id="home">
         <img
           className="logo scale-in-center"
           src={codejam_text}
-          alt="treehacks large text"
+          alt="codejam large text"
         />
 
-        <div className='rowC'>
-
+        <div className="rowC">
           <div id="date-text">
-            April 1-3, 2022
+            Check back in 2023 for our next Code Jam!
             <br />
             George Fox University
-
-          <a rel="noopener noreferrer" target="_blank" href="https://forms.gle/YxomhWQR7J119TaN7" className="apply-button">
-            Join the Board
-          </a>
-
+            {/* <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="/"
+              className="apply-button"
+              aria-disabled
+            >
+              Join the Board
+            </a> */}
           </div>
         </div>
-
       </div>
     );
   }
