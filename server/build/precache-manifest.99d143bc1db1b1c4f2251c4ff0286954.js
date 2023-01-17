0058b27a4b471e3638305e4408bf8f150c3bfeb1
Harry <32529321+HarryZ10@users.noreply.github.com>
2023-01-17 10:56:19 -0800

    Animations (#16)
    * Fix mobile redesign page intro component
    
    * animations add

---
+++ server/build/precache-manifest.99d143bc1db1b1c4f2251c4ff0286954.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b006bc0ef938c912802098d3f8dd6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d226aab8183a0ee8d3b",
+    "url": "/static/css/2.71e3fac3.chunk.css"
+  },
+  {
+    "revision": "1375dc5df9c2b43815ee",
+    "url": "/static/css/main.d4fa887b.chunk.css"
+  },
+  {
+    "revision": "4d226aab8183a0ee8d3b",
+    "url": "/static/js/2.53075adb.chunk.js"
+  },
+  {
+    "revision": "1375dc5df9c2b43815ee",
+    "url": "/static/js/main.cbf969da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "126040753b93e1c5e805d4cee6f78939",
+    "url": "/static/media/BuildingOnly.12604075.png"
+  },
+  {
+    "revision": "4f07e5216dc0fca0d61c7a2752559e8f",
+    "url": "/static/media/asc.4f07e521.png"
+  },
+  {
+    "revision": "3b83a7fba2cccdb9b5a5e6f31f807d0f",
+    "url": "/static/media/faq1.3b83a7fb.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5ba50be15d91ef3cf409a0cbf9cbcfee",
+    "url": "/static/media/georgefox.5ba50be1.png"
+  },
+  {
+    "revision": "5bfe24c2f3e76e7cc3fb6e355b4b109e",
+    "url": "/static/media/silverpine.5bfe24c2.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
