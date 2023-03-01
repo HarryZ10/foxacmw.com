972d0fca7a490421220e97725dd57a6807cb3a95
Harry <32529321+HarryZ10@users.noreply.github.com>
2023-03-01 11:18:23 -0800

    Ad tracks (#19)

--- client/src/js/challenges.jsx
+++ client/src/js/challenges.jsx
@@ -5,7 +5,7 @@ function Challenges() {
   return(
     <div id="challenges">
       <div className="stripe accent-orange"/>
-      <h1 className="section-heading">innovation at fox</h1>
+      <h1 className="section-heading">tracks</h1>
       <div className="section-intro">{challenges_intro}</div>
       <div className="container">
         {challenges.map((challenge, i) => {
@@ -19,7 +19,7 @@ function Challenges() {
 function Challenge(props) {
   return(
     <div className="challenge">
-      <img src={props.logo} alt={props.name + " logo"} />
+      {/* <img src={props.logo} alt={props.name + " logo"} /> */}
       <h1>{props.name}</h1>
       <p>{props.description}</p>
     </div>
